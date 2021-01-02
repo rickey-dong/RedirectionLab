a0de61e4b9326ed9cb90d722c608aaac79e45275
Rickey Dong <rickeyd2004@gmail.com>
2021-01-02 15:01:26 -0500

    modified my starify method, correctly made the scanner parsing in the main, and added the book text file

--- MakeStars.java
+++ MakeStars.java
@@ -1,19 +1,34 @@
 import java.util.*;
 public class MakeStars
 {
+  public static void main(String[] args)
+  {
+    Scanner wholeFile = new Scanner(System.in);
+    while (wholeFile.hasNextLine())
+    {
+      String indivLine = wholeFile.nextLine();
+      Scanner eachLine = new Scanner(indivLine);
+      while (eachLine.hasNext())
+      {
+        String word = eachLine.next();
+        System.out.print(starify(word));
+        if (eachLine.hasNext())
+        {
+          System.out.print(' ');
+        }
+      }
+      if (wholeFile.hasNextLine())
+      {
+        System.out.println();
+      }
+    }
+  }
   public static String starify(String terminalInput)
   {
     String starString = "";
     for (int i = 0; i < terminalInput.length(); i++)
     {
-      if (terminalInput.charAt(i) != ' ')
-      {
         starString += "*";
-      }
-      else
-      {
-        starString += " ";
-      }
     }
     return starString;
   }
