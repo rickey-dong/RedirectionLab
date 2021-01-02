d8aea8885c0b42987038742dc0dc51acb51a7a06
Rickey Dong <rickeyd2004@gmail.com>
2021-01-02 17:18:49 -0500

    added text input to PigLatin, and it worked on sonnet.txt according to expectedOutput.txt

--- PigLatin.java
+++ PigLatin.java
@@ -1,27 +1,27 @@
+import java.util.*;
 public class PigLatin
 {
   public static void main(String[] args)
   {
-    System.out.println(pigLatinSimple("mock"));
-    System.out.println(pigLatinSimple("pie"));
-    System.out.println(pigLatinSimple("david"));
-    System.out.println(pigLatinSimple("aaron"));
-    System.out.println(pigLatinSimple("AARON"));
-    System.out.println("=======================");
-    System.out.println(pigLatin("the"));
-    System.out.println(pigLatin("check"));
-    System.out.println(pigLatin("skee"));
-    System.out.println(pigLatin("emu"));
-    System.out.println(pigLatin("grade"));
-    System.out.println("=======================");
-    System.out.println(pigLatinBest("*emu"));
-    System.out.println(pigLatinBest("4chan"));
-    System.out.println(pigLatinBest("fish!"));
-    System.out.println(pigLatinBest("fish"));
-    System.out.println(pigLatinBest("the."));
-    System.out.println(pigLatinBest("cat!"));
-    System.out.println(pigLatinBest("amazing?"));
-    System.out.println(pigLatinBest("apple%"));
+    Scanner wholeFile = new Scanner(System.in);
+    while (wholeFile.hasNextLine())
+    {
+      String indivLine = wholeFile.nextLine();
+      Scanner eachLine = new Scanner(indivLine);
+      while (eachLine.hasNext())
+      {
+        String word = eachLine.next();
+        System.out.print(pigLatinBest(word));
+        if (eachLine.hasNext())
+        {
+          System.out.print(' ');
+        }
+      }
+      if (wholeFile.hasNextLine())
+      {
+        System.out.println();
+      }
+    }
   }
   public static String pigLatinSimple(String s)
   {
