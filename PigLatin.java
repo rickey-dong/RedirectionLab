0957524e5fc0cf89854c06433dbfcc5dd6c5c9f2
Rickey Dong <rickeyd2004@gmail.com>
2021-01-02 15:35:07 -0500

    created helper method to check if string starts with a digraph

--- PigLatin.java
+++ PigLatin.java
@@ -30,4 +30,22 @@ public class PigLatin
     return s.charAt(0) == 'a' || s.charAt(0) == 'e' || s.charAt(0) == 'i' 
           || s.charAt(0) == 'o' || s.charAt(0) == 'u';
   }
+  public static String pigLatin(String s)
+  {
+    return "S";
+  }
+  public static boolean startsWithDigraph(String s)
+  {
+    String[] digraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl",
+                        "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", 
+                        "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+    for (int i = 0; i < digraphs.length; i++)
+    {
+      if (s.substring(0,2).equals(digraphs[i]))
+      {
+        return true;
+      }
+    }
+    return false;
+  }
 }
